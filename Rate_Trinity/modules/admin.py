11f3f9a4b050adc78c53f26a7dcea85c5483712e
SeanFitz1997 <33702330+SeanFitz1997@users.noreply.github.com>
2018-09-29 23:42:27 +0100

    Register Module class in admin

--- Rate_Trinity/modules/admin.py
+++ Rate_Trinity/modules/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from modules.models import Module
 
-# Register your models here.
+admin.site.register(Module)
--- Rate_Trinity/modules/views.py
+++ Rate_Trinity/modules/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from modules.models import Module
 
 def index(request):
     return render(request, 'modules/modulesList.html')
