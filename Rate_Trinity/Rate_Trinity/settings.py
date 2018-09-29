de6290aca14c61edc5bd8929b0b0f5983ac014d9
SeanFitz1997 <33702330+SeanFitz1997@users.noreply.github.com>
2018-09-29 16:26:35 +0100

    Installing apps

--- Rate_Trinity/Rate_Trinity/settings.py
+++ Rate_Trinity/Rate_Trinity/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'home',
+    'about',
+    'modules',
+    'lecturers',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
