29a08702ac263a8287b1815739716c8bea943670
SeanFitz1997 <33702330+SeanFitz1997@users.noreply.github.com>
2018-09-29 22:49:38 +0100

    Create module model class

--- Rate_Trinity/modules/models.py
+++ Rate_Trinity/modules/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class Module(models.Model):
+    code = models.CharField(max_length=6)
+    name = models.TextField(max_length=100)
+    lecturer = models.TextField(max_length=50)
+    ects = models.PositiveIntegerField(validators=[MinValueValidator(5), MaxValueValidator(20)])
+
+    def __str__(self):
+        return "%s : %s" % (self.code, self.name) 
