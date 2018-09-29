ad1387e3aea3570e73a95d26c8ddf2bb19a4cde1
SeanFitz1997 <33702330+SeanFitz1997@users.noreply.github.com>
2018-09-29 19:12:15 +0100

    Simple header template

--- Rate_Trinity/home/views.py
+++ Rate_Trinity/home/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1>Rate Trinity</h1>\
-                            <p>Please give your opinions on your modules and lecturers</p>')
+    return render(request, 'home/home.html')
