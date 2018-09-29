00e442f1cbb08d0b22bb92fb7becdc311845d3e8
SeanFitz1997 <33702330+SeanFitz1997@users.noreply.github.com>
2018-09-29 22:17:24 +0100

    Created module list page

--- Rate_Trinity/Rate_Trinity/urls.py
+++ Rate_Trinity/Rate_Trinity/urls.py
@@ -19,5 +19,6 @@ from django.conf.urls import url,include
 
 urlpatterns = [
     path('', include('home.urls')),
+    path('modules/', include('modules.urls')),
     path('admin/', admin.site.urls)
 ]
