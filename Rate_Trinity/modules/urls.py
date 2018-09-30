2b73987583b1c0ede5d63aed3062a22c71f02b0c
SeanFitz1997 <33702330+SeanFitz1997@users.noreply.github.com>
2018-09-30 13:22:26 +0100

    Url to veiw Module page & Create simple view module page

--- Rate_Trinity/modules/urls.py
+++ Rate_Trinity/modules/urls.py
@@ -5,5 +5,7 @@ from django.views.generic import ListView, DetailView
 
 urlpatterns = [
     url(r'^$', ListView.as_view(queryset=Module.objects.all().order_by('code'),
-                                template_name='modules/modulesList.html'))
+                                template_name='modules/modulesList.html')),
+    url(r'^(?P<pk>\d+)$', DetailView.as_view(model=Module,
+                                            template_name='modules/viewModule.html'))
 ]
