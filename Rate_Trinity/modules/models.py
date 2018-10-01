0143c79320515907c958a0fd4c03c76039324bf0
SeanFitz1997 <33702330+SeanFitz1997@users.noreply.github.com>
2018-10-01 22:57:53 +0100

    Edit feilds in Modules model & Edit viewModule URL to have code as PK

--- Rate_Trinity/modules/models.py
+++ Rate_Trinity/modules/models.py
@@ -2,10 +2,11 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Module(models.Model):
-    code = models.CharField(max_length=6)
-    name = models.TextField(max_length=100)
-    lecturer = models.TextField(max_length=50)
-    ects = models.PositiveIntegerField(validators=[MinValueValidator(5), MaxValueValidator(20)])
+    code = models.CharField('The modules unique code', max_length=6, primary_key=True)
+    name = models.CharField('The modules full name', max_length=100)
+    lecturer = models.CharField('The name of the modules lecturer', max_length=50)
+    description = models.TextField('The description & learning outcomes of the module',max_length=2000)
+    ects = models.PositiveIntegerField('The ECTS provided on completion of the module', validators=[MinValueValidator(5), MaxValueValidator(20)])
 
     def __str__(self):
         return "%s : %s" % (self.code, self.name) 
--- Rate_Trinity/modules/urls.py
+++ Rate_Trinity/modules/urls.py
@@ -6,6 +6,6 @@ from django.views.generic import ListView, DetailView
 urlpatterns = [
     url(r'^$', ListView.as_view(queryset=Module.objects.all().order_by('code'),
                                 template_name='modules/modulesList.html')),
-    url(r'^(?P<pk>\d+)$', DetailView.as_view(model=Module,
+    url(r'^(?P<pk>[^/]+)$', DetailView.as_view(model=Module,
                                             template_name='modules/viewModule.html'))
 ]
