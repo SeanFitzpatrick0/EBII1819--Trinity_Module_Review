910a49b82a03f400398614a461b464e8f87c9f56
SeanFitz1997 <33702330+SeanFitz1997@users.noreply.github.com>
2018-10-04 17:10:47 +0100

    About page & url to about page

--- Rate_Trinity/Rate_Trinity/urls.py
+++ Rate_Trinity/Rate_Trinity/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('', include('home.urls')),
     path('modules/', include('modules.urls')),
     path('lecturers/', include('lecturers.urls')),
+    path('about/', include('about.urls')),
     path('admin/', admin.site.urls)
 ]
