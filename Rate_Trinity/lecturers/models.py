26a82d8848727bf9cf14de1e0476ad9565256881
SeanFitz1997 <33702330+SeanFitz1997@users.noreply.github.com>
2018-10-07 20:52:22 +0100

    Created Lecturer Model

--- Rate_Trinity/lecturers/models.py
+++ Rate_Trinity/lecturers/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class lecturer(models.Model):
+    name = models.CharField('The Lecturers full name with no professional title', max_length=100)
+    school = models.CharField('The school the lecture works in', max_length=100)
+
+    def __str__(self):
+        return "%s : %s" % (self.name, self.school) 
