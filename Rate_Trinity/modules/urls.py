093c0af2bdda409ee116595e43e61d5093128b68
SeanFitz1997 <33702330+SeanFitz1997@users.noreply.github.com>
2018-10-07 12:13:49 +0100

    Edited href for header nav and gave url paths names

--- Rate_Trinity/modules/urls.py
+++ Rate_Trinity/modules/urls.py
@@ -5,7 +5,7 @@ from django.views.generic import ListView, DetailView
 
 urlpatterns = [
     url(r'^$', ListView.as_view(queryset=Module.objects.all().order_by('code'),
-                                template_name='modules/modulesList.html')),
+                                template_name='modules/modulesList.html'), name='module_list'),
     url(r'^(?P<pk>[^/]+)$', DetailView.as_view(model=Module,
                                             template_name='modules/viewModule.html'))
 ]
