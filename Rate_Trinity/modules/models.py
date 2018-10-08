960f027d85abe4b162e69ee9423406bdde8d1500
SeanFitz1997 <33702330+SeanFitz1997@users.noreply.github.com>
2018-10-08 14:14:26 +0100

    edit to module model

--- Rate_Trinity/modules/models.py
+++ Rate_Trinity/modules/models.py
@@ -4,7 +4,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 class Module(models.Model):
     code = models.CharField('The modules unique code', max_length=6, primary_key=True)
     name = models.CharField('The modules full name', max_length=100)
-    lecturer = models.CharField('The name of the modules lecturer', max_length=50)
+    lecturer = models.CharField('The name of the module lecturer', max_length=50)
     description = models.TextField('The description & learning outcomes of the module',max_length=2000)
     ects = models.PositiveIntegerField('The ECTS provided on completion of the module', validators=[MinValueValidator(5), MaxValueValidator(20)])
 
