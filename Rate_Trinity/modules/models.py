8ee3b48ad96f4dd3239c7fb52f45dac4692562d1
SeanFitz1997 <33702330+SeanFitz1997@users.noreply.github.com>
2018-10-09 11:25:30 +0100

    Edit to modue model to make the lecturer a Foreign Key to the lecturer model

--- Rate_Trinity/modules/models.py
+++ Rate_Trinity/modules/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from lecturers.models import Lecturer
+
+DEFAULT_LECTURER_ID = 1
 
 class Module(models.Model):
     code = models.CharField('The modules unique code', max_length=6, primary_key=True)
     name = models.CharField('The modules full name', max_length=100)
-    lecturer = models.CharField('The name of the module lecturer', max_length=50)
+    lecturer = models.ForeignKey(Lecturer, on_delete=models.CASCADE, default=DEFAULT_LECTURER_ID)
     description = models.TextField('The description & learning outcomes of the module',max_length=2000)
     ects = models.PositiveIntegerField('The ECTS provided on completion of the module', validators=[MinValueValidator(5), MaxValueValidator(20)])
 
