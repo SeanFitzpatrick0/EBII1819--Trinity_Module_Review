a3d7945d6c90b89e82834e011046ef347d249081
SeanFitz1997 <33702330+SeanFitz1997@users.noreply.github.com>
2018-10-11 17:32:25 +0100

    Displaying all comments on the module view

--- Rate_Trinity/modules/urls.py
+++ Rate_Trinity/modules/urls.py
@@ -3,8 +3,18 @@ from . import views
 from modules.models import Module, Module_Comment
 from django.views.generic import ListView, DetailView
 
+class ModuleDetailView(DetailView):
+    context_object_name = 'module'
+    model = Module
+    template_name='modules/viewModule.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comments'] = Module_Comment.objects.all()
+        return context
+
 urlpatterns = [
     url(r'^$', ListView.as_view(queryset=Module.objects.all().order_by('code'),
                                 template_name='modules/modulesList.html'), name='module_list'),
-    url(r'^(?P<pk>[^/]+)$', DetailView.as_view(model=Module, template_name='modules/viewModule.html'), name='module_view')
+    url(r'^(?P<pk>[^/]+)$', ModuleDetailView.as_view(), name='module_view')
 ]
