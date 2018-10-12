0b0d30d51187e22090a6e73caa380c832b659e1f
SeanFitz1997 <33702330+SeanFitz1997@users.noreply.github.com>
2018-10-12 13:37:41 +0100

    Display comments only about the subject module on module page

--- Rate_Trinity/modules/urls.py
+++ Rate_Trinity/modules/urls.py
@@ -10,7 +10,7 @@ class ModuleDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['comments'] = Module_Comment.objects.all()
+        context['comments'] = Module_Comment.objects.filter(subject_id=context['module'].pk)
         return context
 
 urlpatterns = [
