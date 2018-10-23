f6cfcf6cad330ee858516816f3acbdcca58319ff
SeanFitz1997 <33702330+SeanFitz1997@users.noreply.github.com>
2018-10-23 14:52:39 +0100

    Provide error message when submitting invaid form

--- Rate_Trinity/users/views.py
+++ Rate_Trinity/users/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from . forms import UserRegisterForm
+from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
     if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             userName = form.cleaned_data.get('username')
             messages.success(request, f'Account succesfully created for { userName }.')
             return redirect('homepage')
+        else: 
+            error_message = ''
+            feilds_errors = [ (field.label, field.errors) for field in form]
+            for feild in feilds_errors:
+                if feild[1] != '':
+                    error_message = error_message + feild[1]
+
+            messages.warning(request, f'Unable to create user. Your form is not valid.\
+             Please correct the following issues:{error_message}')
     else:
-        form = UserRegisterForm()
+        form = UserCreationForm()
     return render(request, 'users/register.html', {'form' : form})
