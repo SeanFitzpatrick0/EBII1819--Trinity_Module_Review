dae412998d28af0605d172624acdedf199091ea0
SeanFitz1997 <33702330+SeanFitz1997@users.noreply.github.com>
2018-10-24 22:44:59 +0100

    Edits to regitration form

--- Rate_Trinity/users/views.py
+++ Rate_Trinity/users/views.py
@@ -1,27 +1,19 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
+from .forms import RegistrationForm
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             userName = form.cleaned_data.get('username')
             messages.success(request, f'Account succesfully created for { userName }. Please Login')
             return redirect('login_page')
-        else: 
-            error_message = ''
-            feilds_errors = [ (field.label, field.errors) for field in form]
-            for feild in feilds_errors:
-                if feild[1] != '':
-                    error_message = error_message + feild[1]
 
-            messages.warning(request, f'Unable to create user. Your form is not valid.\
-             Please correct the following issues:{error_message}')
     else:
-        form = UserCreationForm()
+        form = RegistrationForm()
     return render(request, 'users/register.html', {'form' : form})
 
 @login_required
