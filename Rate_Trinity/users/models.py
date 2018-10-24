0b205356038e16a126387f00198eb4007ba3741b
SeanFitz1997 <33702330+SeanFitz1997@users.noreply.github.com>
2018-10-24 23:12:50 +0100

    Created & migrated user modle

--- Rate_Trinity/users/models.py
+++ Rate_Trinity/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from modules.models import Module
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    modules = models.ManyToManyField(Module)
+
+    def __str__(self):
+        return f'{self.user} Profile'
