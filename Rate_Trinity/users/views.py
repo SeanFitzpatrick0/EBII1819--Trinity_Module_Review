0192e0d41a12621bca774c345554aede6746ecce
SeanFitz1997 <33702330+SeanFitz1997@users.noreply.github.com>
2018-10-28 17:42:52 +0000

    Display users modules on profile page

--- Rate_Trinity/users/views.py
+++ Rate_Trinity/users/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import RegistrationForm
+from modules.models import Module
+from django.contrib.auth.models import User
 
 def register(request):
     if request.method == 'POST':
