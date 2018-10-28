bc82ef84e23b478b4bdde817436b1100e1432dea
SeanFitz1997 <33702330+SeanFitz1997@users.noreply.github.com>
2018-10-28 18:40:08 +0000

    Display user comments on the profile page

--- Rate_Trinity/users/models.py
+++ Rate_Trinity/users/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
-from modules.models import Module
+from modules.models import Module, Module_Comment
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     modules = models.ManyToManyField(Module)
+    comments = models.ManyToManyField(Module_Comment)
 
     def __str__(self):
         return f'{self.user} Profile'
