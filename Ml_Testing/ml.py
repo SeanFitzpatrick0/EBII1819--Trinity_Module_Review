94ecfd293f82ec62a8e66a9498d1cccfb58047eb
JackDCollins <collinj6@tcd.ie>
2018-11-21 22:54:44 +0000

    Remade class - added new function isAbusive Message & Tested Several phrases

--- Ml_Testing/ml.py
+++ Ml_Testing/ml.py
@@ -2,17 +2,46 @@
 from hatesonar import Sonar
 import json
 
-sonar = Sonar()
-a = sonar.ping(text="I hate this module, lecturer is crap and a real prick")
-
-#obj = json.loads(a)
-print(a)
 
 
-hateSpeech = 1.0
-offensiveLanguage =0.0
+def isAbusiveComment(x):
+    sonar = Sonar()
+    a = sonar.ping(text=x)
+    #print(x)
+    #print(a)
 
-problimit = 0.6
+    hateConfidence = 0.0
+    offenseConfidence = 0.0
+    neitherConfidence = 0.0
 
-if hateSpeech > problimit or offensiveLanguage >problimit :
-    print("limit exceeded - do not post")
+
+    for result in a['classes'] :
+        #print(result)
+        #print(result['confidence'])
+        #print(result["class_name"])
+        if result["class_name"] == "hate_speech" :
+            hateConfidence = result['confidence']
+        if result["class_name"] == "offensive_language" :
+            offenseConfidence = result['confidence']
+        if result["class_name"] == "neither" :
+            neitherConfidence = result['confidence']
+
+    if neitherConfidence > 0.7:
+        return False
+
+    if hateConfidence > 0.4 and offenseConfidence >0.4 :
+        return True
+
+    if hateConfidence > 0.6 and offenseConfidence >0.3 :
+        return True
+
+    if hateConfidence > 0.7 :
+        return True
+
+    if offenseConfidence >0.6 :
+        return True
+
+    return False
+    #print("Hate ",hateConfidence, " || offenseConfidence ",offenseConfidence," || neither ",neitherConfidence)
+
+print(isAbusiveComment("hey"))
