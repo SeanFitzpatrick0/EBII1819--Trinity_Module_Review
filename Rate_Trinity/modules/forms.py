75e8b498d156ecd06e046e67edd97018d0268bcd
SeanFitz1997 <33702330+SeanFitz1997@users.noreply.github.com>
2018-11-29 19:10:21 +0000

    Editing the comment form

--- Rate_Trinity/modules/forms.py
+++ Rate_Trinity/modules/forms.py
@@ -2,6 +2,11 @@ from django.forms import ModelForm
 from . models import Module_Comment
 
 class Module_Comment_Form(ModelForm):
+
+    def __init__(self, *args, **kwargs): 
+        super(ModelForm, self).__init__(*args, **kwargs)
+        self.fields['content'].label = ''
+
     class Meta:
         model = Module_Comment
         fields = ['content']
