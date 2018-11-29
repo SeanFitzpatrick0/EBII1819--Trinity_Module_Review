f244d20976c6d981cd2e5bfaf7de6b9509758f56
SeanFitz1997 <33702330+SeanFitz1997@users.noreply.github.com>
2018-11-29 10:33:15 +0000

    Fixing adding comment bug

--- Rate_Trinity/users/views.py
+++ Rate_Trinity/users/views.py
@@ -4,12 +4,22 @@ from django.contrib.auth.decorators import login_required
 from .forms import RegistrationForm
 from modules.models import Module
 from django.contrib.auth.models import User
+from users.models import Profile
 
 def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            #Created associated profile for user
+            try:
+                Profile.objects.update_or_create(
+                user=user
+            )
+            except:
+                user.delete()
+                pass
+
             userName = form.cleaned_data.get('username')
             messages.success(request, f'Account succesfully created for { userName }. Please Login')
             return redirect('login_page')
